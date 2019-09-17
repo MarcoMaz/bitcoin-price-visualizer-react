ba456b8c2c87b03c5ac46683c9d51f698ad6894d
Marco Mazzai <mazzai.marco@gmail.com>
2019-09-17 12:21:52 +0200

    Updates

---
+++ precache-manifest.ea857ae6a0b92211ff8f297df177bbaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1e53857e78625a027330b25214cef0",
+    "url": "/bitcoin-price-visualizer-react/index.html"
+  },
+  {
+    "revision": "8be1285f7af7a01b82b0",
+    "url": "/bitcoin-price-visualizer-react/static/css/main.cbcddbdd.chunk.css"
+  },
+  {
+    "revision": "926bd1b8011ab795f8b2",
+    "url": "/bitcoin-price-visualizer-react/static/js/2.45060bf4.chunk.js"
+  },
+  {
+    "revision": "8be1285f7af7a01b82b0",
+    "url": "/bitcoin-price-visualizer-react/static/js/main.a6a9ddad.chunk.js"
+  },
+  {
+    "revision": "52ab686670f7670f8eab",
+    "url": "/bitcoin-price-visualizer-react/static/js/runtime~main.2d62e3ff.js"
+  }
+]);
